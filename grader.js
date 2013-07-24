bf3b6a11ec6f9e1635f2c1ffadf2cee63fe73c9d
omarduarte <omarduarte@gmail.com>
2013-07-23 23:08:53 -0400

    added a dot at the end of first heading.

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -65,10 +66,41 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <check_url>', 'Check index from url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+    
+    var html_file_to_open;
+    if(program.url){
+        
+        rest.get(program.url).on('complete', function(result) {    
+            if (result instanceof Error) {
+                console.log('Error: ' + result.message);
+                process.exit(1);
+            }             
+            fs.writeFile('temp.html', result, function(err) {
+                if (err){ 
+                    //debug
+                    console.log('Error thrown');
+                    
+                    throw err;
+                    process.exit(1);
+                }
+                html_file_to_open = 'temp.html';
+                var checkJson = checkHtmlFile(html_file_to_open, program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+            });
+        });
+
+    }
+    else{
+        html_file_to_open = program.file;
+        var checkJson = checkHtmlFile(html_file_to_open, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+} 
+
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
